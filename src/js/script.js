3c896377559778d327780fd0c659b1718455be75
salesluis <salesluis434@gmail.com>
2024-01-02 20:36:08 -0300

    feat: tooltip

--- src/js/script.js
+++ src/js/script.js
@@ -3,9 +3,13 @@ import animationScroll from "./modules/scroll-animacao.js";
 import initTabNav from "./modules/tab-nav.js";
 import initAccordion from "./modules/acordion.js";
 import modal from "./modules/modal.js";
+import initTooltip from "./modules/tooltip.js";
+import initDropdownMenu from "./modules/dropdown-menu.js";
 
 ScrollSuave();
 animationScroll();
 initAccordion();
 initTabNav();
-modal()
+modal();
+initTooltip();
+initDropdownMenu();
